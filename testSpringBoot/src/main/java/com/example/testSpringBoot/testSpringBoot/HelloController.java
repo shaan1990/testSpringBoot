8c7a69632531012fc5574e93762352f468f0304c
iamfr <iamfr@DESKTOP-MI6UVM7>
2020-04-23 17:15:13 +0530

    First Commit All files uploaded

---
+++ testSpringBoot/src/main/java/com/example/testSpringBoot/testSpringBoot/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.testSpringBoot.testSpringBoot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello javaTpoint";
+	}
+}
